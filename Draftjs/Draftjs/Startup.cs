bd4e21f64e40b91357288c998b394b2fdea629b4
Haru <phamlexuanvinh@gmail.com>
2017-01-07 12:46:02 +0700

    Begin

---
+++ Draftjs/Draftjs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Draftjs.Startup))]
+namespace Draftjs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
