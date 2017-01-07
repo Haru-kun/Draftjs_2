d59210f60f85178d6fa4651eba28f55343738635
thuyhongnguyen <nguyennhulan1607@gmail.com>
2017-01-07 14:43:26 +0700

    Thong ke

---
+++ BanhangMVC/BanhangMVC/Controllers/ThongKeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BanhangMVC.Models;
+
+namespace BanhangMVC.Controllers
+{
+    public class ThongKeController : Controller
+    {
+
+        QuanlyBanhangEntities db = new QuanlyBanhangEntities();
+
+        public ActionResult ThongKeWeb()
+        {
+            decimal tkdoanhthu =(decimal) db.ChiTietDonDatHangs.Sum(x => x.SoLuong * x.DonGia).Value;
+
+            ViewBag.SoNguoiTruyCap = (int)HttpContext.Application["SoNguoiTruyCap"];
+            ViewBag.SoNguoiDangTruyCap = (int)HttpContext.Application["SoNguoiDangTruyCap"];
+            ViewBag.TKDT = tkdoanhthu;
+            return PartialView();
+        }
+
+        // Giải phóng bộ nhớ khi không sử dụng
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if(db != null)
+                {
+                    db.Dispose();
+                }
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
